8dc9db24bf4e2b52e001cd1c3b90e5e158a6c1c6
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 21:46:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
+import Home from './ Home';
+import About from './About';
 
 const App = () => {
   return (
