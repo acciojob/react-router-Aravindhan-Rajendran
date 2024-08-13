feaf4c6fea6b309aed5346a68b788d21e09224c0
Aravindhan-Rajendran <aravindhan12072000@gmail.com>
2024-08-14 00:00:42 +0530

    lo

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
-// src/App.js
+// App.js
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 
 const App = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-      </Switch>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,12 @@
-// src/Home.js
+// Home.js
 import React from 'react';
 
 const Home = () => {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
 };
 
 export default Home;
