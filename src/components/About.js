21d7d87ce00cc23902de0a8dab53ed133b3d6c8b
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 21:44:17 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
-// src/About.js
 import React from 'react';
 
 const About = () => {
-  return <h1>About This Application</h1>;
+  return (
+    <div>
+      <h1>About This Application</h1>
+      <p>This application demonstrates basic routing using React Router.</p>
+    </div>
+  );
 };
 
 export default About;
