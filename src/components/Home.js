a7be9c9db4d5c8b84ec735cd7635c76ad75f489c
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-08-13 21:43:57 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
-// src/Home.js
 import React from 'react';
 
 const Home = () => {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
 };
 
 export default Home;
